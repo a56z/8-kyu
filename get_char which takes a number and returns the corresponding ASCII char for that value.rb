bad5534bd6050142c3b1977b7ad21d21fa20beb5
Enrique Folte <hhfolte@gmail.com>
2019-03-20 15:34:39 +0100

    Create get_char which takes a number and returns the corresponding ASCII char for that value.rb

---
+++ get_char which takes a number and returns the corresponding ASCII char for that value.rb
@@ -0,0 +1,13 @@
+=begin
+Get character from ASCII Value
+
+Write a function getChar/GetChar/get_char which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+getChar(65) # => 'A'
+=end
+
+def getChar(c)
+    c.chr
+end
