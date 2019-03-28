b0f5cdbb8f86ac7de1684e50f7c4d0e2177624d7
Enrique Folte <hhfolte@gmail.com>
2019-03-28 17:50:54 +0100

    Create How good are you really.rb

---
+++ How good are you really.rb
@@ -0,0 +1,15 @@
+=begin
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+You got an array with your colleges' points. Now calculate the average to your points!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your classes points. For calculating the average point you may add your point to the given array!
+=end
+
+def better_than_average(arr, points)
+    (arr.sum / arr.length).abs > points ? false : true
+end
+  
