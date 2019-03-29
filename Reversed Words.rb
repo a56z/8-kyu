45cfb71846ccce42e5ead0f27bc6e0608aa1ee1f
Enrique Folte <hhfolte@gmail.com>
2019-03-29 11:11:52 +0100

    Create Reversed Words.rb

---
+++ Reversed Words.rb
@@ -0,0 +1,12 @@
+=begin
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+solution("The greatest victory is that which requires no battle") 
+# should return "battle no requires which that is victory greatest The"
+=end
+
+def solution(sentence)
+  sentence.split.reverse.join(" ")
+end
