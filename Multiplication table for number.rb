26a7fba5a002b2958a897204e567e3b9c29c3347
Enrique Folte <hhfolte@gmail.com>
2019-03-29 10:02:11 +0100

    Update Multiplication table for number.rb

--- Multiplication table for number.rb
+++ Multiplication table for number.rb
@@ -15,6 +15,7 @@ For example, a multiplication table (string) for number == 5 looks like below:
 10 * 5 = 50
 =end
 
+
 def multiTable(number)
     "1 * #{number} = #{1*number}\n2 * #{number} = #{2*number}\n3 * #{number} = #{3*number}\n4 * #{number} = #{4*number}\n5 * #{number} = #{5*number}\n6 * #{number} = #{6*number}\n7 * #{number} = #{7*number}\n8 * #{number} = #{8*number}\n9 * #{number} = #{9*number}\n10 * #{number} = #{10*number}"
   end
