d35dd2ca0df326899629acc9402ab272dee6267c
Enrique Folte <hhfolte@gmail.com>
2019-04-01 12:23:02 +0200

    Create Grasshopper - Basic Function Fixer.rb

---
+++ Grasshopper - Basic Function Fixer.rb
@@ -0,0 +1,12 @@
+=begin
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+=end
+
+
+def addFive(num)
+  res = num + 5
+  return res
+end
