373b369b7b4a2f1b184f65764f4d7d77086ea286
Enrique Folte <hhfolte@gmail.com>
2019-04-03 10:46:58 +0200

    Create Get number from string.rb

---
+++ Get number from string.rb
@@ -0,0 +1,19 @@
+=begin
+Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+Function:
+
+####javascript
+
+getNumberFromString(s)
+####ruby
+
+get_number_from_string(s)
+####CSharp
+
+GetNumberFromString(string s)
+=end
+
+def get_number_from_string(s)
+  s.scan(/\d/).join('').to_i
+end
