8b3a9b56e87496b84ed0f67c13517dc19e96b2f1
Enrique Folte <hhfolte@gmail.com>
2019-04-04 19:17:44 +0200

    Create Input Array of elements.rb

---
+++ Input Array of elements.rb
@@ -0,0 +1,14 @@
+=begin
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+=end
+
+
+def printArray(array)
+  array.join(",")
+end
