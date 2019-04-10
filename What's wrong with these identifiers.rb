0b4214515a8dc741006102ce494fb3acefd03139
Enrique Folte <hhfolte@gmail.com>
2019-04-10 15:39:10 +0200

    Create What's wrong with these identifiers.rb

---
+++ What's wrong with these identifiers.rb
@@ -0,0 +1,9 @@
+#An identifier is simply a name...
+#Can you amend this object so that its properties comprise of vaild identifiers?
+
+Person = {
+  "1stname" => "John",
+  "second-name" => "Doe",
+  "email@ddress" => "john.doe@email.com",
+  "male.female" => "M"
+}
