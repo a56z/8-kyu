b20008e67fd91f789632e8799699dc0d1d369f18
Enrique Folte <hhfolte@gmail.com>
2019-04-13 10:09:22 +0200

    Create Convert number to reversed array of digits.rb

---
+++ Convert number to reversed array of digits.rb
@@ -0,0 +1,15 @@
+=begin
+Convert number to reversed array of digits
+Given a random number:
+
+C#: long;
+C++: unsigned long;
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+=end
+
+def digitize(n)
+n.to_s.split("").reverse.map(&:to_i)
+end
