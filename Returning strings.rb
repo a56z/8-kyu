e3b04c540f7a6e31892244c2c9b02b5a77154e9c
Enrique Folte <hhfolte@gmail.com>
2019-04-16 22:34:05 +0200

    Create Returning strings.rb

---
+++ Returning strings.rb
@@ -0,0 +1,10 @@
+=begin
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+*[Make sure you type the exact thing I wrote or the program may not execute properly]*
+=end
+
+
+def greet(name)
+  "Hello, #{name} how are you doing today?"
+end
