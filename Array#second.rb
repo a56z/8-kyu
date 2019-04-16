eafcbc001b17d7f1e182999fd8b38cede4c3ab86
Enrique Folte <hhfolte@gmail.com>
2019-04-16 13:12:56 +0200

    Create Array#second.rb

---
+++ Array#second.rb
@@ -0,0 +1,15 @@
+=begin
+Define a new instance method on the Array class called second, which returns the second item in an array (similar to the way .first and .last work in Ruby).
+
+If there is no element with index 1 in the array, return nil.
+
+Examples
+[3, 4, 5].second  #  => 4 
+[].second         #  => nil
+=end
+
+Class Array
+  def second
+    self[1]
+  end
+end
